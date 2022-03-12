e77ddd9f6686129e860db93630a623ea6724297e
Parker <pknopf@ucsd.edu>
2022-03-12 09:08:45 -0800

    password logic

--- source/find_shape.py
+++ source/find_shape.py
@@ -1,13 +1,17 @@
+from cv2 import threshold
 import jetson.inference
 import jetson.utils
 
 import argparse
 import sys
 
+import random
+import time
+
 class FindShape():
 	'''Distinguish Shape through visual imaging'''
 
-	def __init__():
+	def __init__(self):
 		# parse the command line
 		self.parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.")
 
@@ -31,38 +35,88 @@ class FindShape():
 		self.input = jetson.utils.videoSource(self.opt.input_URI, argv=sys.argv)
 		self.output = jetson.utils.videoOutput(self.opt.output_URI, argv=sys.argv)
 
+	def determineClass(self):
+		# capture the next image
+		img = input.Capture()
+
+		# detect objects in the image (with overlay)
+		detections = self.net.Detect(img, overlay=self.opt.overlay)
+
+		# print the detections
+		# print("detected {:d} shapes in image".format(len(detections)))
+
+		# for detection in detections:
+		# 	print(detection)
+
+		# update the title bar
+		self.output.SetStatus("{:s} | Network {:.0f} FPS".format(self.opt.network, self.net.GetNetworkFPS()))
+
+		# print out performance info
+		self.net.PrintProfilerTimes()
+
+		# exit on input/output EOS
+		if not self.input.IsStreaming() or not self.output.IsStreaming():
+			return
+		
+		return detections
+
+class Password():
+
+	def __init__(self):
+		self.findShape = FindShape()
+
+		self.num = 2
+		self.parameters = ['circle', 'square']
+		self.password = self.createPassword(self.num, self.parameters)
+
+
+	def createPassword(self, num, parameters):
+		password = []
+		for i in range(0, num):
+			password[i] = random.choice(parameters)
+		return password
+
+	def checkPassword(self, attempt):
+		if attempt == self.password:
+			return True
+
+	def getshape(self):
+		print(self.findShape.determineClass())
+		return self.findShape.determineClass()
+
 	def main(self):
-		'''Main Loop'''
+		'''main running loop'''
+
 		try:
-			while True:
-				# capture the next image
-				img = input.Capture()
+			do = 1
+			while do:
+				print('\nA new password has been created!\n')
+				self.createPassword(self.num, self.parameters)
 
-				# detect objects in the image (with overlay)
-				detections = net.Detect(img, overlay=opt.overlay)
+				print('Try to guess the password now by placing either a circle or square in the frame of the camera.')
 
-				# print the detections
-				print("detected {:d} shapes in image".format(len(detections)))
-
-				for detection in detections:
-					print(detection)
-
-				# render the image
-				output.Render(img)
-
-				# update the title bar
-				output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
-
-				# print out performance info
-				net.PrintProfilerTimes()
-
-				# exit on input/output EOS
-				if not input.IsStreaming() or not output.IsStreaming():
-					break
+				attempt= []
+				for i in range(0, self.num):
+					while self.getshape == 'null':
+						pass
+					time_start = time.time()
+					while self.getshape != 'null':
+						attempt[i] = self.getshape()
+					time_stop = time.time()			
+					threshold = time_stop - time_start
+					if threshold < 1:
+						i = i - 1
+				
+				if self.checkPassword(attempt):
+					print("Succefuly guessed the password!")
+					do = 0
+				else:
+					print("Password incorrect, try again")
 
 		except KeyboardInterrupt:
 			pass
 
+
 if __name__ == "__main__":
-	findshape = FindShape()
-	findshape.main()
+	password = Password()
+	password.main()
