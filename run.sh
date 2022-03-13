7be7213848f1d8783c1c61999b6a7e1238780c32
Parker <pknopf@ucsd.edu>
2022-03-13 11:11:54 -0700

    files

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 find_shape.py --model=resnet18.onnx --labels=labels.txt --input_blob=input_0 --output_blob=output_0 /dev/video0
+python3 find_shape.py --model=models/shapes/ssd-mobilenet.onnx --labels=models/shapes/labels.txt --input_blob=input_0 --output-cvg=scores -output-bbox=boxes /dev/video0
